4ba2384372ff665647d11fbdd824c314ce29041f
Juan Manuel Infante Quiroga <juan.infante12@hotmail.com>
2024-06-28 19:41:56 -0500

    Creacion de componentes

--- admin/src/views/RequestPassword.vue
+++ admin/src/views/RequestPassword.vue
@@ -1,13 +1,8 @@
 <template>
 
-    <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-      <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-        <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
-        <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Recupera tu Contraseña</h2>
-    </div>
-  
-    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form class="space-y-6" action="#" method="POST">
+<GuestLayout title="Recupera tu Contraseña">
+
+    <form class="space-y-6" action="#" method="POST">
         <div>
             <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Correo</label>
             <div class="mt-2">
@@ -23,12 +18,15 @@
         <div>
             <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Enviar</button>
         </div>
-        </form>
+    </form>
 
-    </div>
-</div>
+</GuestLayout>
+
 </template>
 
+<script setup>
+    import GuestLayout from "../components/GuestLayout.vue";
+</script>
 
 <style scoped>
 
--- admin/src/views/ResetPassword.vue
+++ admin/src/views/ResetPassword.vue
@@ -1,13 +1,8 @@
 <template>
 
-    <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-      <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-        <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
-        <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Crea una nueva Contraseña</h2>
-    </div>
-  
-    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form class="space-y-6" action="#" method="POST">
+<GuestLayout title="Crea una nueva Contraseña">
+
+    <form class="space-y-6" action="#" method="POST">
         <div>
             <label for="new-password" class="block text-sm font-medium leading-6 text-gray-900">Nueva Contraseña</label>
             <div class="mt-2">
@@ -24,17 +19,21 @@
             </div>
         </div>  
         <div class="text-sm">
-            <router-link :to="{name: 'login'}" class="font-semibold text-indigo-600 hover:text-indigo-500">Regresar al Inicio de Sesion</router-link>
+            <router-link :to="{name: 'login'}" class="font-semibold text-indigo-600 hover:text-indigo-500">Regresar aInicio de Sesion</router-link>
         </div>
         <div>
             <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Enviar</button>
         </div>
-        </form>
+    </form>
 
-    </div>
-</div>
+</GuestLayout> 
+
+
 </template>
 
+<script setup>
+    import GuestLayout from "../components/GuestLayout.vue";
+</script>
 
 <style scoped>
 
